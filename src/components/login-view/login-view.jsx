64e7a7870078c535cfdd4ea73da892181f18e2cb
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-05-11 13:42:43 +0200

    Update quotation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -16,11 +16,11 @@ export function LoginView(props) {
       <form>
         <label>
           Username:
-          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+          <input type='text' value={username} onChange={e => setUsername(e.target.value)} />
         </label>
         <label>
           Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
         </label>
         <button type='submit' onClick={handleSubmit}>Submit</button>
       </form>
