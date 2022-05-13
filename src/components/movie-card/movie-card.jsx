0e0721fde30dab5c969db5b598796dc3f405bf13
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-05-13 09:16:38 +0200

    Implement bootstrap card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
 
   render() {
     const { movie, onMovieClick } = this.props;
     {/*Add click event to call back function in MainView to change state */ }
-    return <div className='movie-card' onClick={() => {
-      onMovieClick(movie);
-    }}  >{movie.title}</div>
+    return (
+      <Card>
+        <Card.Img variant='top' src={movie.imageUrl} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.plot}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant='link'>Details</Button>
+        </Card.Body>
+      </Card>
+    )
   }
 }
 
