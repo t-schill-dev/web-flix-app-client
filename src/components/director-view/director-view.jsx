d78714dea4f274836425312d33099108ce99aa75
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-05-24 11:21:19 +0200

    Add styling to director view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -8,30 +8,26 @@ export class DirectorView extends React.Component {
   render() {
     const { director, onBackClick } = this.props;
     return (
-      <>
-        <Container className='view-container'>
-          <Row id='movie-director'>
 
-            <Card >
-              <div>
-                <Card.Header className='movie-title'>{director.name}</Card.Header>
-                <Button id='return-button' onClick={() => { onBackClick(); }}>Back</Button>
-              </div>
-              <Card.Body >
-                <div>
-                  <label htmlFor='bio' className='label'>Bio: </label>
-                  <Card.Text className='value'>{director.bio}</Card.Text>
-                </div>
-                <div>
-                  <label htmlFor='birth' className='label'>Birth: </label>
-                  <Card.Text className='value'>{director.birth}</Card.Text>
-                </div>
-              </Card.Body>
-            </Card>
 
-          </Row>
-        </Container>
-      </>
+      <Row id='movie-director'>
+
+        <Card >
+          <div>
+            <Card.Header className='movie-title'>{director.name}</Card.Header>
+            <Button id='return-button' onClick={() => { onBackClick(); }}>Back</Button>
+          </div>
+          <Card.Body >
+            <label htmlFor='bio' className='label'>Biography: </label>
+            <p className='value'>{director.bio}</p>
+            <label htmlFor='birth' className='label'>Birth: </label>
+            <p className='value'>{director.birth}</p>
+          </Card.Body>
+        </Card>
+
+      </Row>
+
+
     )
   }
 }
