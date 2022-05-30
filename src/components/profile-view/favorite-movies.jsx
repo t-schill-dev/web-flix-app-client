30e51c80a87fa40b998031b87d6419ce87493b9b
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-05-30 15:23:15 +0200

    Update favorite Movies View

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,34 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Col, Row, Card, Button } from 'react-bootstrap';
+import { Col, Row, Card, Button, Carousel, CarouselItem } from 'react-bootstrap';
 
-export function FavoriteMovies({ favoriteMovies, removeFav }) {
+export function FavoriteMovies({ favoriteMovies, removeFav, movies }) {
+
+
+  const favoriteMoviesId = favoriteMovies.map(m => m._id)
+
+  const favoriteMoviesList = movies.filter(m => {
+    return favoriteMoviesId.includes(m._id)
+  })
+
   return (
     <>
       <Row>
         <Col>
-          <h4>Favorite Movies</h4>
+          <h4 className='text-dark' style={{ padding: 10 }}>Favorite Movies</h4>
         </Col>
       </Row>
       <Row>
-        {favoriteMovies.map(movie => {
-          return (
-            <Col key={_id}>
-              <Card id='movie-card'>
-                <Card.Img fluid='true' id='movie-card_img' className='text-center' variant='top' src={movie.imageUrl} />
-                <Card.Body>
-                  <Card.Title>{movie.title}</Card.Title>
-                  <Card.Subtitle className='text-muted'>{movie.year}</Card.Subtitle>
-                  <Button variant='outline-danger' onClick={() => removeFav(movie._id)}>Remove from Favorites</Button>
-                  <Link to={`/movies/${movie._id}`}>
-                    <Button id='details-btn' variant='link'>Details</Button>
-                  </Link>
 
-                </Card.Body>
-              </Card>
-            </Col>
-          )
-        })}
+        <Card>
+          <Carousel variant='dark' className='movie-carousel'>
+            {favoriteMoviesList.length === 0 ? (
+              <p>You have no favorite movies yet</p>
+            ) : (
+              favoriteMoviesList.map(movie => {
+                return (
+                  <Carousel.Item key={movie._id} className='text-center' >
+                    <Card.Title className='text-dark'>{movie.title}</Card.Title>
+                    <img
+                      id='mini-movie-card_img'
+                      src={movie.imageUrl}
+                      alt="movie poster"
+                    />
+                  </Carousel.Item>
+
+                )
+              })
+            )
+            }
+          </Carousel>
+          <Button variant='outline-danger' onClick={() => removeFav(movies._id)}>Remove from Favorites</Button>
+        </Card>
+
+
       </Row>
     </>
   )
