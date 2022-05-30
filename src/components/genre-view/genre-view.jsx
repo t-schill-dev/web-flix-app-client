eed0fcce2c2f6f21bff630765a173c83e2c05ff5
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-05-30 09:42:55 +0200

    Finish styling of GenreView

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -14,33 +14,35 @@ export class GenreView extends React.Component {
     return (
       <>
         <Container >
-          <Row id='genre-view'>
-            <Card >
+          <Row >
+            <Card id='genre-view'>
               <div>
                 <Card.Header className='genre-title'>{genreName}</Card.Header>
+
                 <Button id='return-button' onClick={() => { onBackClick(); }}>Back</Button>
               </div>
-              <Card.Text className='text-center'>More movies of the same genre:</Card.Text>
+              <Card.Text className='text-muted subtitle'>More movies of the same genre:</Card.Text>
+              <Carousel variant='dark' className='movie-carousel'>
+                {movies.map((movie) => {
+                  if (movie.genres.includes(genreName)) {
+                    return (
+                      <Carousel.Item key={movie._id} className='text-center' >
+                        <img
+                          id='mini-movie-card_img'
+                          src={movie.imageUrl}
+                          alt="movie poster"
+                        />
+                      </Carousel.Item>
+                    )
+                  }
+                }
+                )}
+              </Carousel>
             </Card>
           </Row>
 
 
-          <Carousel >
-            {movies.map((movie) => {
-              if (movie.genres.includes(genreName)) {
-                return (
-                  <Carousel.Item key={movie._id} className='text-center' >
-                    <img
-                      id='mini-movie-card_img'
-                      src={movie.imageUrl}
-                      alt="movie poster"
-                    />
-                  </Carousel.Item>
-                )
-              }
-            }
-            )}
-          </Carousel>
+
         </Container>
       </>
     )
